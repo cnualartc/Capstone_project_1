c063a14cfc712ba20edd1d4c4542a06378246e6c
Christian Nualart <nualart@Christians-Mac-mini.local>
2023-02-17 15:38:57 -0600

    Attempt one at fixing linter errors

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-
 const moreBands = document.querySelectorAll('.hide-mobile');
 const moreButton = document.querySelector('.more-button');
 const lessButton = document.querySelector('.less-button');
 
-
-function showBands () {
+function showBands() {
   moreBands.forEach((e) => {
     e.classList.add('show-mobile');
     moreButton.classList.add('hide-mobile');
   });
 }
 
-function hideBands () {
+function hideBands() {
   moreBands.forEach((e) => {
     e.classList.remove('show-mobile');
     moreButton.classList.remove('hide-mobile');
-  })
+  });
 }
 
-moreButton.addEventListener("click", e => {
+moreButton.addEventListener('click', (e) => {
   showBands();
-})
+});
 
-lessButton.addEventListener("click", e => {
+lessButton.addEventListener('click', (e) => {
   hideBands();
-})
+});
